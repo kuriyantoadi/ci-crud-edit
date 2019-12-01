48aaa93e84de7048540a99474987a4dca908d42f
kuriyantoadi <melatikuning212@gmail.com>
2019-12-01 20:52:03 +0700

    update

---
+++ application/views/main.php
@@ -0,0 +1,31 @@
+<html>
+
+<title>HTML Tutorial</title>
+<body>
+<h1>This is a heading</h1>
+<?php echo anchor('C_buku/i','Tambah data') ?>
+<table border="1">
+ <tr>
+  <td><b>No</b></td>
+  <td><b>Judul</b></td>
+  <td><b>Tebal</b></td>
+  <td><b>Penerbit</b></td>
+  <td><b>Aksi</b></td>
+ </tr>
+
+ <?php
+ $i=1;
+ foreach ($buku as $row) {
+  $no=$i++;
+ ?>
+  <tr>
+  <td><?php echo $no?></td>
+  <td><?php echo $row->judul;?></td>
+  <td><?php echo $row->tebal;?></td>
+  <td><?php echo $row->penerbit;?></td>
+  <td><?php echo anchor('C_buku/e/'.$row->id,'Edit')?></td>
+  </tr>
+  <?php } ?>
+</table>
+</body>
+</html>
